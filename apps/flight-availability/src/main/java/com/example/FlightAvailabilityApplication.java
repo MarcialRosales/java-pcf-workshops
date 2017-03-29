e0650dd39863909a63ddc3496760b94ffde546bf
Marcial Rosales <mrosales@pivotal.io>
2017-03-29 12:52:29 +0200

    added cloud connector and updated flight availability to use the connector

--- apps/flight-availability/src/main/java/com/example/FlightAvailabilityApplication.java
+++ apps/flight-availability/src/main/java/com/example/FlightAvailabilityApplication.java
@@ -7,12 +7,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.service.ServiceInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.domain.Flight;
 import com.example.domain.FlightRepository;
+
+import io.pivotal.demo.cups.cloud.WebServiceInfo;
 
 @SpringBootApplication
 public class FlightAvailabilityApplication {
@@ -27,11 +33,39 @@ public class FlightAvailabilityApplication {
 	}
 
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FlightAvailabilityApplication.class, args);
 	}
 }
 @Configuration
+@Profile({"cloud"})
+class CloudConfig  {
+	
+	@Bean
+	Cloud cloud() {
+		return new CloudFactory().getCloud();
+	}
+	
+    @Bean
+    public WebServiceInfo fareServiceInfo(Cloud cloud) {
+        ServiceInfo info = cloud.getServiceInfo("fare-service");
+        if (info instanceof WebServiceInfo) {
+        	return (WebServiceInfo)info;
+        }else {
+        	throw new IllegalStateException("fare-service is not of type WebServiceInfo. Did you miss the tag attribute?");
+        }
+    }
+    
+    @Bean(name = "fareService")
+	public RestTemplate fareService(RestTemplateBuilder builder, WebServiceInfo fareService) {
+		return builder.basicAuthorization(fareService.getUserName(), fareService.getPassword()).rootUri(fareService.getUri()).build();
+		
+	}
+}
+
+@Configuration
+@Profile("{!cloud}")
 @ConfigurationProperties(prefix = "fare-service")
 class FareServiceConfig {
 	String uri;
